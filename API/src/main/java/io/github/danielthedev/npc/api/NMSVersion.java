89952471630782eac973254b0d219d4e9a7735a5
BuildTools <unconfigured@null.spigotmc.org>
2021-12-17 13:48:41 +0100

    moved NMS version from API to NPCFactory module

--- API/src/main/java/io/github/danielthedev/npc/api/NMSVersion.java
+++ API/src/main/java/io/github/danielthedev/npc/api/NMSVersion.java
@@ -10,15 +10,4 @@ public enum NMSVersion {
     v1_17_R1,
     v1_18_R1;
 
-    private static final NMSVersion VERSION;
-
-    static {
-        String packageName = Bukkit.getServer().getClass().getPackage().getName();
-        String version = packageName.substring(packageName.lastIndexOf('.') + 1);
-        VERSION = NMSVersion.valueOf(version);
-    }
-
-    public static NMSVersion getNMSVersion() {
-        return VERSION;
-    }
 }
--- NPCFactory/src/main/java/io/github/danielthedev/npc/NPCFactory.java
+++ NPCFactory/src/main/java/io/github/danielthedev/npc/NPCFactory.java
@@ -2,18 +2,27 @@ package io.github.danielthedev.npc;
 
 import io.github.danielthedev.npc.api.NMSVersion;
 import io.github.danielthedev.npc.api.NPC;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 import java.util.UUID;
 
-public class NPCManager {
+public class NPCFactory {
+
+    private static final NMSVersion VERSION;
+
+    static {
+        String packageName = Bukkit.getServer().getClass().getPackage().getName();
+        String version = packageName.substring(packageName.lastIndexOf('.') + 1);
+        VERSION = NMSVersion.valueOf(version);
+    }
 
     public static NPC createNPC(Location location, String displayName) {
         return createNPC(UUID.randomUUID(), location, displayName);
     }
 
     public static NPC createNPC(UUID uuid, Location location, String displayName) {
-        return switch (NMSVersion.getNMSVersion()) {
+        return switch (getNMSVersion()) {
             case v1_16_R1 -> null;
             case v1_16_R2 -> null;
             case v1_16_R3 -> null;
@@ -21,5 +30,8 @@ public class NPCManager {
             case v1_18_R1 -> new io.github.danielthedev.npc.nms.v1_18_R1.CraftNPC(uuid, location, displayName);
         };
     }
-
+    ¬
+    public static NMSVersion getNMSVersion() {
+        return VERSION;
+    }
 }
